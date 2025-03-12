8021e903d323cf9f357e893f4b500f5cf0d6cb93
Sajan <epicslooyer@gmail.com>
2025-03-12 10:59:24 -0700

    Fixed global.css error to do with bg

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,7 +19,6 @@ export default function RootLayout({ children }) {
       <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
-          <Toaster />
         </ThemeProvider>
       </body>
     </html>
