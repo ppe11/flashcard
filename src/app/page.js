58a5da47fd17125ebbc4831f0eb90a25af5edbe2
ppe11 <panphooeain@Pans-MacBook-Air-4.local>
2025-03-16 04:52:42 -0700

    Update page.js and add Home.jsx component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 'use client'
 
 import Navbar from "@/components/Navbar";
+import Home from "@/components/Home";
 
 export default function Home() {
   return (
     <>
     <Navbar></Navbar>
+    <Home></Home>
     </>
   );
 }
