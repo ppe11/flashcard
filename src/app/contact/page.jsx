cab1f7261bef92dc70ce7b5a16b06dee109ac89d
Oaksoe11 <osk@sfu.ca>
2025-03-17 00:15:32 -0700

    Update placeholder text color to less gray

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -35,7 +35,7 @@ const ContactPage = () => {
     <div className="flex flex-col justify-between bg-orange-50 px-6">
      <div className="flex flex-grow justify-center items-center py-24">
         <div className="bg-orange-200 p-6 md:p-8 rounded-2xl shadow-2xl w-full max-w-md h-auto sm:max-w-lg md:max-w-xl">
-          <h2 className="text-center text-2xl md:text-3xl font-bold mb-5">Send Us a Message!</h2>
+          <h2 className="text-center text-2xl md:text-3xl font-semibold mb-5">Send Us a Message!</h2>
           
           <form className="space-y-4" onSubmit={onSubmit}>
             {/* First Name & Last Name - Responsive Grid */}
@@ -44,15 +44,16 @@ const ContactPage = () => {
                 <label className="block text-md font-medium text-gray-800 py-2">First Name</label>
                 <input 
                   type="text" placeholder='Enter first name' required name='firstname'
-                  className="w-full p-3 bg-orange-50 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-400"
-                />
+                  className="w-full p-3 bg-orange-50 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-400
+                  placeholder-gray-400"/>
               </div>
 
               <div>
                 <label className="block text-md font-medium text-gray-800 py-2">Last Name</label>
                 <input 
                   type="text" placeholder='Enter last name' required name='lastname'
-                  className="w-full p-3 border border-gray-300 bg-orange-50 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-400"
+                  className="w-full p-3 border border-gray-300 bg-orange-50 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-400
+                  placeholder-gray-400"
                 />
               </div>
             </div>
@@ -62,7 +63,8 @@ const ContactPage = () => {
               <label className="block text-md font-medium text-gray-800 py-2">Email</label>
               <input 
                 type="email" placeholder='Enter your email' required name='email'
-                className="w-full p-3 border border-gray-300 bg-orange-50 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-400"
+                className="w-full p-3 border border-gray-300 bg-orange-50 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-400
+                placeholder-gray-400"
               />
             </div>
 
@@ -70,7 +72,8 @@ const ContactPage = () => {
             <div>
               <label className="block text-md font-medium text-gray-800 py-2">Message</label>
               <textarea placeholder='Enter your message' required name='message'
-                className="w-full p-3 border border-gray-300 bg-orange-50 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-400 h-32 resize-none"
+                className="w-full p-3 border border-gray-300 bg-orange-50 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-400 h-32 resize-none
+                placeholder-gray-400"
               />
             </div>
 
