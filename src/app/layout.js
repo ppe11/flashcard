b3f082e63183193465fa0f098d8c8532fd1ceb92
Oaksoe11 <osk@sfu.ca>
2025-03-18 07:59:00 -0700

    Fix the footer covering content on extra large screens

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
        <Navbar/>
        <main className="flex-grow">{children}</main>
