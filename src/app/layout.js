250b2a30d71c2ff14ce70b7ca6909ce2e0c16787
Oaksoe11 <osk@sfu.ca>
2025-03-18 05:39:08 -0700

    Add configuration for shadcn Tab component

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
        <Navbar/>
-        {children}
+       <main className="flex-grow">{children}</main>
         <Footer/>
       </body>
     </html>
