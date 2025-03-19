2a950ef77bb6f06679f1e59a6afb29d3c5161bba
ppe11 <ppe1@sfu.ca>
2025-03-18 23:11:19 -0700

    Implement hovering animation and adjust some spacing

--- src/app/quiz/page.jsx
+++ src/app/quiz/page.jsx
@@ -24,27 +24,28 @@ const Quiz = () => {
 
   return (
     <div className="w-full flex flex-col justify-center items-center bg-orange-50 min-h-screen">
-      <h2 className="text-2xl font-semibold mb-6">Choose your desired pet to take a quiz!</h2>
+      <h2 className="text-2xl font-semibold p-10">Choose your desired pet to take a quiz!</h2>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 auto-rows-min">
         {pets.map((pet, index) => (
           <Card
             key={index}
-            className={`cursor-pointer shadow-md hover:shadow-xl transition duration-300 bg-orange-100 px-4 pt-3 pb-0 text-center rounded-xl flex flex-col 
-              ${pet.name === "or.....Let us decide!" ? "h-49 w-full md:col-span-2" : "h-49 w-44"}`}
+            className={`cursor-pointer shadow-md hover:shadow-2xl hover:bg-orange-300 transition duration-300 bg-orange-100 
+              px-4 pt-3 text-center rounded-xl flex flex-col 
+              ${pet.name === "or.....Let us decide!" ? "h-49 w-full md:col-span-2" : "h-49 w-50"}`}
             onClick={() => handleClick(pet.path)}
           >
-          <CardContent className="text-lg font-medium py-2">{pet.name}</CardContent>
+          <CardContent className="text-xl font-medium py-2">{pet.name}</CardContent>
           <div className="flex-grow flex items-end justify-center mt-auto min-h-0">
-            <Image 
-              src={pet.image} 
-              alt={pet.name} 
-              width={100}  
-              height={100}  
-              className="object-contain"
-            />
-          </div>
-          </Card>
+          <Image 
+            src={pet.image} 
+            alt={pet.name} 
+            width={pet.name === "or.....Let us decide!" ? 400 : 100}  
+            height={pet.name === "or.....Let us decide!" ? 400 : 100}  
+            className="object-contain"
+          />
+        </div>
+        </Card>
         ))}
       </div>
     </div>
