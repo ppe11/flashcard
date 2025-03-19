ffa13686342f7148ed192dbc385e014a003a5c95
Oaksoe11 <osk@sfu.ca>
2025-03-19 00:09:21 -0700

    Create shelters page with API data containers

--- src/app/shelters/page.jsx
+++ src/app/shelters/page.jsx
@@ -2,13 +2,31 @@
 
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React from 'react';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { motion } from "framer-motion";
+
+/**
+ * Replace with API integration
+ */
+
+const shelters = [
+    { id: 1, name: "##Shelter Name##", contact: "(888) 888-888", location: "8888 88 Ave", hours: "8am - 8pm" },
+    { id: 2, name: "##Shelter Name##", contact: "(888) 888-888", location: "8888 88 Ave", hours: "8am - 8pm" },
+    { id: 3, name: "##Shelter Name##", contact: "(888) 888-888", location: "8888 88 Ave", hours: "8am - 8pm" },
+    { id: 4, name: "##Shelter Name##", contact: "(888) 888-888", location: "8888 88 Ave", hours: "8am - 8pm" },
+    { id: 5, name: "##Shelter Name##", contact: "(888) 888-888", location: "8888 88 Ave", hours: "8am - 8pm" },
+    { id: 6, name: "##Shelter Name##", contact: "(888) 888-888", location: "8888 88 Ave", hours: "8am - 8pm" },
+  ];
+
 
 const Shelters = () => {
   return (
-     <div>
-
-     </div>
+    <div className="max-w-6xl mx-auto p-6">
+    
+  </div>
   )
 }
 
