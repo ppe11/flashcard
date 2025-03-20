ec5d7f2330bd2307300c80552a0b1c1ca5db8f2a
ppe11 <ppe1@sfu.ca>
2025-03-20 00:14:15 -0700

    Add padding on top

--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import { Card, CardHeader, CardContent, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 
 const ResultsPage = () => {
@@ -14,28 +15,30 @@ const ResultsPage = () => {
   }, []);
 
   return (
-    <div className="w-full text-center bg-orange-50 min-h-screen p-10">
-      <h2 className="text-xl font-semibold mb-6">Recommended Pets</h2>
+    <div className="w-full text-center min-h-screen pt-32">
+      <h2 className="text-2xl font-semibold mb-6">Your Perfect Pet Awaits! 🐾</h2>
 
       {pets.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 py-6 px-16">
           {pets.map((pet, index) => (
-            <div key={index} className="shadow-sm hover:shadow-xl transition duration-600 bg-white rounded-xl p-6 text-center">
-              <div className="flex justify-center">
-                <img src={pet.image} alt={pet.name} className="w-32 h-32 object-contain rounded-md" />
-              </div>
-              <h3 className="text-lg font-semibold mt-4">{pet.name}</h3>
-              <ul className="text-sm text-gray-600 mt-2">
-                {pet.description.map((desc, i) => (
-                  <li key={i}>• {desc}</li>
-                ))}
-              </ul>
-              <div className="mt-4">
+            <Card key={index} className="shadow-sm hover:shadow-xl transition duration-600 rounded-xl bg-orange-50">
+              <CardHeader className="flex items-center justify-center p-4">
+                <img src={pet.image} alt={pet.name} className="w-32 h-32 object-contain rounded-md"/>
+              </CardHeader>
+              <CardContent className="text-center">
+                <h3 className="text-lg font-semibold">{pet.name}</h3>
+                <ul className="text-sm text-gray-600 mt-2">
+                  {pet.description.map((desc, i) => (
+                    <li key={i}>• {desc}</li>
+                  ))}
+                </ul>
+              </CardContent>
+              <CardFooter className="flex justify-center p-4 pb-6">
                 <Button className="bg-orange-500 hover:bg-orange-600 text-white text-lg border rounded-3xl p-5">
                   Adopt me!
                 </Button>
-              </div>
-            </div>
+              </CardFooter>
+            </Card>
           ))}
         </div>
       ) : (
