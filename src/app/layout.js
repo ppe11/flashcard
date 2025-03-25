ca12770ce7c21a22ae75fc0b10752b3b45f520e8
Ashton Windows <ironslamhead@gmail.com>
2025-03-25 16:53:28 -0700

    Cleared public and changed favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import {Suspense} from 'react';
+import Head from 'next/head';
 
 
 
@@ -25,6 +26,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
