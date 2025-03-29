c22819d7407d1cebec03b173adb0d9190c3bb6ec
Ashton Linux <ironslamhead@gmail.com>
2025-03-28 20:06:58 -0700

    Fixed Route error to hopefully pass deploy

--- src/app/pets/api/[id]/route.ts
+++ src/app/pets/api/[id]/route.ts
@@ -1,20 +1,21 @@
 import { fetchPetDetails } from '@/lib/petfinder';
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  request: Request, 
+  { params }: { params: { id: string } }
+) {
   try {
-    
-    const { id } = await params;
-    
-    
+    const id = params.id;
+
     const data = await fetchPetDetails(id);
 
-    console.log("API Response:", data)
+    console.log("API Response:", data);
     
     return new Response(JSON.stringify({ pet: data.animal }), {
       headers: { 'Content-Type': 'application/json' }
     });
   } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
+    return new Response(JSON.stringify({ error: (error as Error).message }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' }
     });
