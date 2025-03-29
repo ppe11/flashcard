baffbd7adf2522095a26018b62be852962dcc6df
Ashton Linux <ironslamhead@gmail.com>
2025-03-28 20:21:08 -0700

    Potentially fixed route for deploy

--- src/app/pets/api/[id]/route.ts
+++ src/app/pets/api/[id]/route.ts
@@ -1,22 +1,18 @@
 import { fetchPetDetails } from '@/lib/petfinder';
-import { NextRequest } from 'next/server';
 
-export async function GET(
-  request: NextRequest, 
-  context: { params: { id: string } }
-) {
+export async function GET(request: Request, { params }: { params: { id: string } }) {
   try {
-    const id = context.params.id; // Correct way to access params
-
+    
+    const { id } = params;
+    
+    
     const data = await fetchPetDetails(id);
-
-    console.log("API Response:", data);
     
     return new Response(JSON.stringify({ pet: data.animal }), {
       headers: { 'Content-Type': 'application/json' }
     });
   } catch (error) {
-    return new Response(JSON.stringify({ error: (error as Error).message }), {
+    return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' }
     });
