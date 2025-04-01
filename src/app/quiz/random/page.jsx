dcc5f014479307b1baed778d51eeea13aaa4d7d5
ppe11 <ppe1@sfu.ca>
2025-03-31 23:34:26 -0700

    Fix random quiz to display the right pet

--- src/app/quiz/random/page.jsx
+++ src/app/quiz/random/page.jsx
@@ -9,8 +9,8 @@ const randomQuestions = [
     question: "How much time can you spend interacting with your pet daily?",
     options: [
       { label: "Several hours—I want a pet that loves attention!", value: ["dog", "cat"] },
-      { label: "Some interaction is nice, but I also want an independent pet", value: ["cat", "small-furry"] },
-      { label: "Occasional interaction, but not daily handling", value: ["scales-fins-other"] },
+      { label: "Some interaction is nice, but I also want an independent pet", value: ["cat", "small-pets"] },
+      { label: "Occasional interaction, but not daily handling", value: ["scales-fins-other", "bird", "fish"] },
     ],
     image: assets.generalpets,
     multiple: false,
@@ -20,8 +20,8 @@ const randomQuestions = [
     question: "How much care are you willing to provide?",
     options: [
       { label: "Daily care, playtime, and training!", value: ["dog", "cat"] },
-      { label: "Regular feeding and cleaning, but minimal training", value: ["small-furry", "bird"] },
-      { label: "Low maintenance—feeding and occasional habitat upkeep", value: ["scales-fins-other"] },
+      { label: "Regular feeding and cleaning, but minimal training", value: ["small-pets", "cats"] },
+      { label: "Low maintenance—feeding and occasional habitat upkeep", value: ["scales-fins-other", "fish", "bird"] },
     ],
     image: assets.generalpets,
     multiple: false,
@@ -31,8 +31,8 @@ const randomQuestions = [
     question: "Do you want a pet that can freely roam your home?",
     options: [
       { label: "Yes! I want a pet that can explore and interact", value: ["dog", "cat"] },
-      { label: "Sometimes—I'd like a pet that can come out occasionally", value: ["small-furry"] },
-      { label: "No, I prefer a pet that stays in its enclosure", value: ["bird"] },
+      { label: "Sometimes—I'd like a pet that can come out occasionally", value: ["small-pets"] },
+      { label: "No, I prefer a pet that stays in its enclosure", value: ["bird",  "fish", "scales-fins-other"] },
     ],
     image: assets.generalpets,
     multiple: false,
@@ -41,10 +41,10 @@ const randomQuestions = [
   {
     question: "What kind of companionship are you looking for?",
     options: [
-      { label: "A loyal, affectionate companion", value: ["dog"] },
-      { label: "A pet that’s independent but still loving", value: ["cat"] },
-      { label: "A quiet pet that I can observe and enjoy", value: ["dog"] },
-      { label: "A small, cute pet that’s fun to watch and sometimes handle", value: ["small-furry"] },
+      { label: "A loyal, affectionate companion", value: ["dog", "cat"] },
+      { label: "A pet that’s independent but still loving", value: ["cat", "bird"] },
+      { label: "A quiet pet that I can observe and enjoy", value: ["bird", "fish", "small-pets"] },
+      { label: "A small, cute pet that’s fun to watch and sometimes handle", value: ["small-pets", "scales-fins-other"] },
     ],
     image: assets.generalpets,
     multiple: false,
@@ -53,9 +53,9 @@ const randomQuestions = [
   {
     question: "How important is your pet’s cleanliness to you?",
     options: [
-      { label: "Very important—I prefer a pet that is clean and easy to maintain", value: ["dog", "cat"] },
-      { label: "Somewhat important—I don’t mind occasional cleaning but prefer a tidy pet", value: ["cat"] },
-      { label: "Not very important—I’m okay with a pet that may require more cleaning", value: ["small-furry"] },
+      { label: "Very important—I prefer a pet that is clean and easy to maintain", value: [ "cat", "fish", "bird"] },
+      { label: "Somewhat important—I don’t mind occasional cleaning but prefer a tidy pet", value: ["cat", "fish", "bird"] },
+      { label: "Not very important—I’m okay with a pet that may require more cleaning", value: ["dog"] },
     ],
     image: assets.generalpets,
     multiple: false,
